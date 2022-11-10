32136ab4198119fc6860e1f092558ccb03e2e7de
Enmanuelph98 <enmanuelph98@gmail.com>
2022-11-10 01:01:41 +0000

    fixed

--- src/models.py
+++ src/models.py
@@ -13,8 +13,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
@@ -64,19 +63,5 @@ class Character(Base):
             "homeworld": homeworld.self
         }
 
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
 
-#     def to_dict(self):
-#         return {}
-
-## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
